73ba1eaea76a6842b0db525e0398635853e6e5ce
James Coglan <jcoglan@gmail.com>
2011-01-09 15:30:03 +0000

    Fix Nodeunit in the browser by not using CommonJS format.

--- nodeunit/object_spec.js
+++ nodeunit/object_spec.js
@@ -1,16 +1,9 @@
-objectSpec = {
+this.ObjectSpec = {
   testSomething: function(test) {
     var object = {hello: "world"}
     test.deepEqual( object, {hello: "world"} )
     test.notDeepEqual( object, {say: "hi"} )
     test.done()
   }
-}
-
-if (typeof exports === 'object') {
-  exports.testSomething = objectSpec.testSomething
-  
-} else if (typeof define === 'function') {
-  define(function(require) { return objectSpec })
 }
 
