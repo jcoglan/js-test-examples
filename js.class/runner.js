435f2ec2420186984a7107fe790ea5ae9c9c32b2
James Coglan <jcoglan@gmail.com>
2012-02-06 02:38:15 +0000

    Change SpecHelper dependency.

--- js.class/runner.js
+++ js.class/runner.js
@@ -5,7 +5,7 @@ JS.Packages(function() { with(this) {
   autoload(/^.*$/, {from: CWD})
   
   file('http://code.jquery.com/jquery-1.7.1.min.js').provides('jQuery')
-  file(CWD + '/js.class/spec_helper.js').provides('SpecHelper').requires('JS.Test')
+  file(CWD + '/js.class/spec_helper.js').provides('SpecHelper').requires('jQuery')
 }})
 
 JS.require('JS.Test', 'SpecHelper', 'jQuery', function() {
